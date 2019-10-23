9e68fffaf08a071b6146e7dfc3f3f3302a768ed7
ccyznl <2845843140@qq.com>
2019-10-23 23:26:28 +0800

    Initial commit

---
+++ loan-admin/src/main/java/com/znlccy/loan/admin/LoanAdminApplication.java
@@ -0,0 +1,18 @@
+package com.znlccy.loan.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @fileName: LoanAdminApplication
+  * @comment: 贷款后台启动类
+  * @author        @date        @version        @descriptions
+  * znlccy         2019/10/23    1.0.0
+  */
+@SpringBootApplication
+public class LoanAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanAdminApplication.class, args);
+    }
+}
---
+++ loan-admin/src/main/java/com/znlccy/loan/admin/controller/RoleController.java
@@ -0,0 +1,10 @@
+package com.znlccy.loan.admin.controller;
+
+/**
+  * @fileName: RoleController
+  * @comment: 角色控制器
+  * @author        @date        @version        @descriptions
+  * znlccy         2019/10/23    1.0.0
+  */
+public class RoleController {
+}
